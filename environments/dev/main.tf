a8dcbe5aa1a5573b345726d01556470b525be79a
Steven Castro <castrostevenyc@gmail.com>
2024-04-01 19:25:58 -0400

    Dev env

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -6,22 +6,28 @@ module "vpc" {
   source = "../../modules/vpc"
 }
 
-module "cicd" {
-  source              = "../../modules/cicd"
-  aws_ecs_cluster     = "simple-setup-ecs-cluster"
-  service_name        = module.assertion_consumer_service.service_name
-  aws_lb_listener_arn = module.alb.listener_arn
-  aws_lb_target_group = module.alb.target_group_arn
-}
+# module "cicd" {
+#   source              = "../../modules/cicd"
+#   aws_ecs_cluster     = "simple-setup-ecs-cluster"
+#   service_name        = module.assertion_consumer_service.service_name
+#   aws_lb_listener_arn = module.alb.listener_arn
+#   aws_lb_target_group = module.alb.target_group_arn
+# }
 
 module "ecs" {
-  source       = "../../modules/ecs_cluster"
-  cluster_name = "simple-setup-ecs-cluster"
-  vpc_id       = module.vpc.vpc_id
+  source                          = "../../modules/ecs_cluster"
+  cluster_name                    = "simple-setup-ecs-cluster"
+  vpc_id                          = module.vpc.vpc_id
+  load_balancer_security_group_id = module.alb.security_group_id
 }
 
 module "simple_setup_web_app" {
-  source = "../../modules/web-app"
+  source                 = "../../modules/web-app"
+  ecs_cluster_id         = module.ecs.cluster_id
+  ecs_tasks_sg_id        = module.ecs.ecs_tasks_sg_id
+  target_group_arn       = module.alb.target_group_arn
+  ecs_execution_role_arn = module.ecs.ecs_execution_role_arn
+  private_subnet_ids     = module.vpc.private_subnet_ids
 }
 
 module "alb" {
@@ -33,7 +39,7 @@ module "alb" {
 
 module "assertion_consumer_service" {
   source                 = "../../modules/assertion-consumer-service"
-  public_subnet_ids      = module.vpc.public_subnet_ids
+  private_subnet_ids     = module.vpc.private_subnet_ids
   vpc_id                 = module.vpc.vpc_id
   ecs_cluster_id         = module.ecs.cluster_id
   ecs_execution_role_arn = module.ecs.ecs_execution_role_arn
